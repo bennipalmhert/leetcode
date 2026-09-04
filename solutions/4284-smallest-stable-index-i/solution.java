bb6ff111a615c82d690536c60f1a733bfbd089b5
Benedikt Palmhert <153665288+bennipalmhert@users.noreply.github.com>
2026-09-04 21:51:14 +0000

    [LeetCode Sync] Runtime - 1 ms (99.79%), Memory - 46.2 MB (88.21%)

---
+++ solutions/4284-smallest-stable-index-i/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] suffixMin = new int[n];
+
+        int min = Integer.MAX_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            min = Math.min(min, nums[i]);
+            suffixMin[i] = min;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            if (max - suffixMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
